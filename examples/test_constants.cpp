dee2d11bdae6972d036d77d3fe26a25d438c9302
Louwrens van Dellen <lthvd@protonmail.com>
2021-10-20 16:30:24 +0200

    Fixup double precision has <16 digits

--- examples/test_constants.cpp
+++ examples/test_constants.cpp
@@ -6,9 +6,9 @@ int main(int argc, const char* argv[])
 {
     double electron_mass = codata::electron_mass;
     double PI = M_PI;
-    // Double values can have max 15-17 digits of precision (53-bit significand precision)
+    // Double values can have max ~15.95 digits of precision (53-bit significand precision)
     // Use G formatting to print as many as needed given the constant's value
-    printf("   %10.17G\n", electron_mass);
-    printf("   %10.17G\n", PI);
+    printf("   %10.15G\n", electron_mass);
+    printf("   %10.15G\n", PI);
     return 0;
 }
